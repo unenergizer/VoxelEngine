230c44926f1887039aab18464e279ff1f34270e1
unenergizer <unenergizer@gmail.com>
2023-07-22 21:05:10 -0500

    Introduce basic antialiasing.

--- lwjgl3/src/main/java/com/mmobuilder/voxel/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/mmobuilder/voxel/lwjgl3/Lwjgl3Launcher.java
@@ -22,6 +22,8 @@ public class Lwjgl3Launcher {
 //        configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
         configuration.setWindowedMode(1280, 720);
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        int samples = 2; // you can also play around with higher values like 4
+        configuration.setBackBufferConfig(8, 8, 8, 8, 16, 0, samples); // 8, 8, 8, 8, 16, 0 are default values
         return configuration;
     }
 }
